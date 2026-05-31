1b70f1eb9889465609178420c6e37f2718922321
kushalhomeocare-khc <kushalhomeocare@gmail.com>
2026-05-31 23:49:26 +0530

    Update payment-success.js

--- api/payment-success.js
+++ api/payment-success.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
 
 res.writeHead(302, {
 Location:
-"https://pay.kushalonline.com/treatment-success.html"
+"https://pay.kushalonline.com/payment-success.html"
 });
 
 res.end();
