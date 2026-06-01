da4c299d7696b04641e9167ab2f8adfd5f9d29f1
kushalhomeocare-khc <kushalhomeocare@gmail.com>
2026-06-01 15:34:05 +0530

    Update payment-failure.js

--- api/payment-failure.js
+++ api/payment-failure.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
 
 res.writeHead(302, {
 Location:
-"https://kushalonline.com/payment-failure"
+"https://pay.kushalonline.com/payment-failure.html"
 });
 
 res.end();
